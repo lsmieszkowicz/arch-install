25608868682fdf708eaa413c9df951f7307538f4
Lukasz <l.smieszkowicz@gmail.com>
2019-07-30 20:02:45 +0200

    Installation of audio packages

---
+++ 05-audio.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo
+echo 'INSTALLING AUDIO PACKAGES'
+echo 
+
+PACKAGES = (
+
+    'alsa-utils' 
+    'pulseaudio' 
+    'pulseaudio-alsa' 
+    'pavucontrol'
+
+)
+
+for PKG in "${PACKAGES[@]}" ; do 
+    echo "Installing: $PKG "
+    pacman -S --no-confirm $PKG
+done
+
+amixer sset Master unmute
+amixer sset Speaker unmute
+amixer sset Headphone unmute
+
+echo "Audio packages installed"
