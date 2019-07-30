e364970af289b5db85758e86311fe839df19e0a3
Lukasz <l.smieszkowicz@gmail.com>
2019-07-30 20:02:31 +0200

    Installation of network packages

---
+++ 04-network.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo
+echo 'INSTALLING NETWORK PACKAGES'
+echo 
+
+PACKAGES = (
+
+    'networkmanager'
+
+)
+
+for PKG in "${PACKAGES[@]}" ; do 
+    echo "Installing: $PKG "
+    pacman -S --no-confirm $PKG
+done
+
+systemctl enable NetworkManager.service
+systemctl start  NetworkManager.service
+
+echo "Network packages installed and enabled"
